aa0c165f01dca43d01d24fea307fd6b92e3bde88
unknown <Jhansi108>
2023-06-06 18:14:14 +0530

    first commit

---
+++ week2-task2/CharacterpatternProgram.java
@@ -0,0 +1,19 @@
+package task2;
+
+public class CharacterpatternProgram {
+
+	public static void main(String[] args) {
+		int letter=65;
+		for(int i=0;i<=9;i++)
+		{
+			for(int j=0;j<=i;j++)
+			{
+				System.out.print((char)letter+"");
+			}
+			letter++;
+			System.out.println();
+		}
+
+	}
+
+}
---
+++ week2-task2/NumberpatternProgram.java
@@ -0,0 +1,21 @@
+package task2;
+
+public class NumberpatternProgram {
+
+	public static void main(String[] args) {
+		int rows=9;
+		for(int i=1;i<=rows;i++)
+		{
+			int num=i;
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print(num+"");
+				num=num+rows-j;
+			}
+			System.out.println();
+			}
+		}
+			
+	}
+
+
