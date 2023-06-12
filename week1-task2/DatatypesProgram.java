102ca066c9cc239d62237a1e26220239b18cc0c8
unknown <Jhansi108>
2023-06-12 10:20:16 +0530

    first commit

---
+++ week1-task2/DatatypesProgram.java
@@ -0,0 +1,21 @@
+package task1;
+
+public class DatatypesProgram {
+static int i=10;
+static float f=2;
+static double d=5.6;
+static char c='A';
+static boolean b=true;
+static long l=12345678;
+static String name="Jhanu";
+	public static void main(String[] args) {
+		System.out.println(i);
+		System.out.println(f);
+		System.out.println(d);
+		System.out.println(c);
+		System.out.println(b);
+		System.out.println(l);
+		System.out.println(name);
+	}
+
+}
---
+++ week1-task2/NestedforloopProgram.java
@@ -0,0 +1,18 @@
+package task1;
+
+public class NestedforloopProgram {
+
+	public static void main(String[] args) {
+		
+		for(int row=1;row<=3;row++)
+		{
+			for(int column=1;column<=row;column++)
+			{
+				System.out.print(row);
+			}
+			System.out.println();
+		}
+
+	}
+
+}
