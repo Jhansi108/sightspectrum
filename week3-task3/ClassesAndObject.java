e62801d5ea530b1ffceea87d98590f3ad97d72a0
unknown <Jhansi108>
2023-06-14 17:25:10 +0530

    first commit

---
+++ week3-task3/ClassesAndObject.java
@@ -0,0 +1,11 @@
+package task3;
+
+public class ClassesAndObject {
+int i=10;
+	public static void main(String[] args) {
+		ClassesAndObject myobj=new ClassesAndObject();
+		System.out.println(myobj.i);
+		
+	}
+
+}
---
+++ week3-task3/PreDefined.java
@@ -0,0 +1,9 @@
+package task3;
+
+public class PreDefined {
+
+	public static void main(String[] args) {
+		System.out.println("Small:"+Math.max(4,8));
+	}
+
+}
