753e085107f5e6d5af5b4f2eec7b6a19bd0187b2
unknown <Jhansi108>
2023-06-19 18:21:51 +0530

    first commit

---
+++ week4-task1/Inheritance.java
@@ -0,0 +1,16 @@
+package task1;
+
+public class Inheritance {
+
+	public static void main(String[] args) {
+		System.out.println("monday");
+	}
+}
+class b extends Inheritance
+{
+	public void print()
+	{
+		System.out.println("raining");
+	}
+
+}
---
+++ week4-task1/StringProgram.java
@@ -0,0 +1,12 @@
+package task1;
+
+public class StringProgram {
+
+	public static void main(String[] args) {
+		String s="jhansi";
+		String t=new String("hello");
+		System.out.println(s);
+		System.out.println(t);
+	}
+
+}
