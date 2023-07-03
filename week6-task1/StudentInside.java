0912c5f0f810e689bd0caecbde995b51d4299bb4
unknown <Jhansi108>
2023-07-03 17:49:53 +0530

    first commit

---
+++ week6-task1/StudentInside.java
@@ -0,0 +1,16 @@
+package practise;
+
+public class StudentInside {
+	int id=11432;
+	String name="jhansi";
+
+	public static void main(String[] args) {
+		StudentInside s1=new StudentInside();
+		System.out.println(s1.id);
+		System.out.println(s1.name);
+		
+		
+
+	}
+
+}
---
+++ week6-task1/StudentOutside.java
@@ -0,0 +1,17 @@
+package practise;
+
+public class StudentOutside {
+	int id=11432;
+	String name="jhansi";
+	class TestStudent1{
+
+	public static void main(String[] args) {
+		StudentOutside s1=new StudentOutside();
+		System.out.println(s1.id);
+		System.out.println(s1.name);
+	}
+
+	}
+
+}
+
